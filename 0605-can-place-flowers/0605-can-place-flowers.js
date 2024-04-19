f0fad2462065989858b70700033f8c768420c5c2
Jin <58104623+JeeIn-Shin@users.noreply.github.com>
2024-04-20 04:53:47 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    let cnt = n;
+    let cur = 0;
+    let lastIdx = flowerbed.length - 1;
+
+    while (cur <= lastIdx && cnt !== 0) {
+        let pre = cur - 1;
+        let next = cur + 1;
+
+        if (cur === 0) {
+                if (flowerbed[cur] === 0 && next <= lastIdx && flowerbed[next] === 0) {
+                    flowerbed[cur] = 1;
+                    cnt--;
+                }
+        }
+        else if (cur === lastIdx)    {
+            if (flowerbed[cur] === 0 && flowerbed[pre] === 0)   {
+                flowerbed[cur] = 1;
+                cnt--;
+            }
+
+        }
+        else {
+            if (flowerbed[cur] === 0 && next <= lastIdx && pre >= 0 && flowerbed[next] === 0 && flowerbed[pre] === 0)   {
+                flowerbed[cur] = 1;
+                cnt--;                
+            }
+        }
+        cur++;
+    }
+    if (cnt === 0)
+        return true;
+    return false;
+};
